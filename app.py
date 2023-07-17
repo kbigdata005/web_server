69f38df29e1ff606f32a2987ea8f96b7db030c0d
Your Name <you@example.com>
2023-07-17 12:27:39 +0900

    POST , GET step 1

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask , render_template
+from flask import Flask , render_template ,request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/' , methods=['GET','POST'])
 def index():
+    if request.method == "GET":
+        os_info = dict(request.headers)
+        print(os_info) 
+        return render_template('index.html',header=os_info )
     
-    return render_template('index.html')
+    elif request.method == "POST":
+        return render_template('index.html',header= "안녕하세요 김태경입니다.!!!")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
